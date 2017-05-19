64259518cefc81bab54ab96a2ebd640bacbec5e2
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-05-19 15:40:50 +0200

    Showing 6 projects by default with a possibility to expand/collapse projects list.

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -5,13 +5,9 @@ import jump from 'jump.js';
 import Project from './Project';
 import projectsContent from './../contentProjects';
 
-function Projects(props) {
-    const filteredProjects = projectsContent.filter(project => (
-        project.technologies.indexOf(props.selectedTechnology) !== -1
-    ));
-
-    const projectsList = (filteredProjects.length ? filteredProjects : projectsContent)
-        .map(project => (
+class Projects extends React.Component {
+    static buildProjectsList(projects) {
+        return projects.map(project => (
             <Project
                 key={project.name}
                 name={project.name}
@@ -21,46 +17,107 @@ function Projects(props) {
                 url={project.url}
                 repository={project.repositoryUrl}
             />
-    ));
+        ));
+    }
 
-    const filteredMessage = (
-        <div className="row-padded-top">
-            <p className="text-center">
-                Showing projects made with {props.selectedTechnology}.&nbsp;
-                <a
-                    href="#projects"
-                    className="link-technology tip--top tip--large"
-                    onClick={() => {
-                        props.onFilterReset();
-                        jump('.js-jump-Projects');
-                    }}
-                >
-                    Show all.
-                </a>
-            </p>
-        </div>
-    );
+    constructor() {
+        super();
+        this.state = { view: 'collapsed' };
+    }
 
-    return (
-        <div>
-            <span id="projects" className="anchor js-jump-Projects" />
-            <section className="row row-section">
-                <div className="row">
-                    <h2 className="text-center">Projects</h2>
-                    {props.selectedTechnology ? filteredMessage : null}
+    toggleView() {
+        const currentView = this.state.view === 'collapsed' ? 'expanded' : 'collapsed';
+        this.setState({ view: currentView });
+    }
+
+    displayProjectsList() {
+        const viewSwitch = (
+            <div className="row-padded-top">
+                <p className="text-center">
+                    <a
+                        href="#projects"
+                        className="link-technology tip--top tip--large"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            this.toggleView();
+                        }}
+                    >
+                        {this.state.view === 'collapsed' ? 'Show more' : 'Show less' }
+                    </a>
+                </p>
+            </div>
+        );
+        let projectsToDisplay;
+
+        if (this.props.selectedTechnology) {
+            projectsToDisplay = projectsContent.filter(project => (
+                project.technologies.indexOf(this.props.selectedTechnology) !== -1
+            ));
+        } else {
+            projectsToDisplay = projectsContent;
+        }
+
+        if (projectsToDisplay.length > 6) {
+            return (
+                <div>
+                    <div className="row">
+                        {
+                            this.state.view === 'collapsed' ?
+                                this.buildProjectsList(projectsToDisplay.slice(0, 6)) :
+                                this.buildProjectsList(projectsToDisplay)
+                        }
+                    </div>
+                    <div className="row">
+                        {viewSwitch}
+                    </div>
                 </div>
-                <div className="row">
+            );
+        }
+        return (
+            <div className="row">
+                {this.buildProjectsList(projectsToDisplay)}
+            </div>
+        );
+    }
+
+    render() {
+        const filteredMessage = (
+            <div className="row-padded-top">
+                <p className="text-center">
+                    Showing projects made with {this.props.selectedTechnology}.&nbsp;
+                    <a
+                        href="#projects"
+                        className="link-technology tip--top tip--large"
+                        onClick={() => {
+                            this.props.onFilterReset();
+                            jump('.js-jump-Projects');
+                        }}
+                    >
+                        Show all.
+                    </a>
+                </p>
+            </div>
+        );
+
+        return (
+            <div>
+                <span id="projects" className="anchor js-jump-Projects" />
+                <section className="row row-section">
+                    <div className="row">
+                        <h2 className="text-center">Projects</h2>
+                        {this.props.selectedTechnology ? filteredMessage : null}
+                    </div>
                     <CSSTransitionGroup
                         transitionName="projects-list-easing"
                         transitionEnterTimeout={500}
                         transitionLeaveTimeout={300}
                     >
-                        {projectsList}
+                        {this.displayProjectsList()}
                     </CSSTransitionGroup>
-                </div>
-            </section>
-        </div>
-    );
+                </section>
+            </div>
+        );
+    }
 }
 
 Projects.propTypes = {
